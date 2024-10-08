6f27a76a6d9a0cc7c539ba8969d43ac4c6e50ec6
lcpmgh <lcpmgh@gmail.com>
2024-10-08 10:12:13 +0800

    更新至v2.0

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ library(ggplot2)
 library(magrittr)
 library(stringr)
 library(purrr)
+library(htmltools)
+
 ui <- dashboardPage(
   dashboardHeader(title = "科研绘图配色推荐器",titleWidth = "100%"),
   dashboardSidebar(disable = TRUE, collapsed = TRUE),
@@ -15,13 +17,17 @@ ui <- dashboardPage(
     tags$hr(),
     tags$div(align = "center", 
              tags$p("\ua9 2021-2024, LIANG Chen, Institute of Mountain Hazards and Environment, CAS. All rights reserved.", style="height:8px"),
-             actionLink(inputId = "", label = "Github", icon = icon("github"), onclick ="window.open('https://github.com/lcpmgh')"),
-             tags$p("  ", style = "display:inline;white-space:pre"),
-             tags$p("Email: lcpmgh@gmail.com", style="display:inline;white-space:pre"),  
+             tags$div(align = "center",
+                      actionLink(inputId = "", label = "lcpmgh ", icon = icon("github"), onclick ="window.open('https://github.com/lcpmgh')"),
+                      tags$p("  ", style = "display:inline;white-space:pre"),
+                      actionLink(inputId = "", label = "lcpmgh@gmail.com", icon = icon("envelope"), onclick ="window.location.href='mailto:lcpmgh@gmail.com'"),
+                      tags$p("  ", style = "display:inline;white-space:pre"),
+                      actionLink(inputId = "", label = "lcpmgh.com", icon = icon("home"), onclick ="window.location.href='http://lcpmgh.com/'")
+             ),
              tags$div(align = "center",
                       tags$a("冀ICP备2022003075号", target="_blank", href="https://beian.miit.gov.cn", style="color:#06c; display:inline;"),
                       tags$p("  ", style = "display:inline;white-space:pre"),
-                      tags$img(src="gaba.png"),
+                      # tags$img(src="gaba.png"),
                       tags$a("川公网安备51010702002736", target="_blank", href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51010702002736", style="color:#06c; display:inline;")
              )
     )
@@ -30,7 +36,7 @@ ui <- dashboardPage(
 
 server <- function(input, output, session){
   # 数据和函数
-  colors <- readLines("@colors.txt") %>% str_split(",")             #读取并识别颜色
+  colors <- readLines("@colors.txt") %>% str_split(",") %>% .[!duplicated(.)]             #读取并识别颜色
   colors_nasc  <- map_int(colors, length) %>% order() %>% colors[.] #按子颜色数量排序
   colors_table <- data.frame(col_id=1:length(colors_nasc), col_num=map_int(colors_nasc, length)) #颜色序号数量表
   examp_plot   <- function(id, colors_nasc=NULL, custom=c("#F5A889", "#ACD6EC")){
@@ -49,9 +55,18 @@ server <- function(input, output, session){
       geom_bar(color="black", stat = "identity")+
       scale_y_continuous(expand = c(0,0,0,0.1))+
       scale_fill_manual(values = tcolor)+
-      labs(x="x-axis", y="y-axis")+
-      theme_bw()
-    # 2.point
+      labs(x="x-axis", y="y-axis", title = "Bar Chart with outlines")+
+      theme_bw()+theme(plot.title = element_text(hjust = 0.5))
+    # 2.box
+    dat_box <- data.frame(a=sample(letters, ncolor, replace = F),
+                          b=runif(ncolor*20, 7, 10))
+    p_box <- ggplot(dat_box, aes(a, b, fill=a))+
+      stat_boxplot(geom = "errorbar", linewidth=0.8, width = 0.3)+
+      geom_boxplot()+
+      scale_fill_manual(values = tcolor)+
+      labs(x="x-axis", y="y-axis", title = "Boxplot with outlines")+
+      theme_bw()+theme(plot.title = element_text(hjust = 0.5))
+    # 3.point
     dat_point <- data.frame(a=rep(runif(30), ncolor),
                             b=rep(runif(30), ncolor),
                             t=rep(sample(letters, ncolor, replace = F), 30))
@@ -59,28 +74,19 @@ server <- function(input, output, session){
       geom_point(shape=21, size=5)+
       scale_color_manual(values = tcolor)+
       scale_fill_manual(values = tcolor)+
-      labs(x="x-axis", y="y-axis")+
-      theme_bw()
-    # 3.box
-    dat_box <- data.frame(a=sample(letters, ncolor, replace = F),
-                          b=runif(ncolor*20, 7, 10))
-    p_box <- ggplot(dat_box, aes(a, b, fill=a))+
-      stat_boxplot(geom = "errorbar", linewidth=0.8, width = 0.3)+
-      geom_boxplot()+
-      scale_fill_manual(values = tcolor)+
-      labs(x="x-axis", y="y-axis")+
-      theme_bw()
-    # 5.line
+      labs(x="x-axis", y="y-axis", title = "scatterplot without outlines")+
+      theme_bw()+theme(plot.title = element_text(hjust = 0.5))
+    # 4.line
     dat_line <- data.frame(a=rep(1:20, ncolor),
                            b=rep(1:ncolor, each=20)+rnorm(20*ncolor, 0, 0.3), 
                            t=rep(sample(letters, ncolor), each=20))
     p_line <- ggplot(dat_line, aes(a, b, color=t, group=t))+
       geom_line(linewidth=1)+
       scale_color_manual(values = tcolor)+
-      labs(x="x-axis", y="y-axis")+
-      theme_bw()
+      labs(x="x-axis", y="y-axis", title = "Line chart without outlines")+
+      theme_bw()+theme(plot.title = element_text(hjust = 0.5))
     # 合并
-    p <- grid.arrange(p_bar, p_line, p_point, p_box, padding=0, nrow = 2, ncol = 2)
+    p <- grid.arrange(p_bar, p_box, p_point, p_line, padding=0, nrow = 2, ncol = 2)
     return(p)
   }            #函数，画4个案例图
   iscolors     <- function(str){
@@ -103,9 +109,9 @@ server <- function(input, output, session){
         HTML(paste("<h5 style='display: inline;'>(数据库内现有", length(colors_nasc), "种配色方案)</h5>"))),
       radioButtons(inputId  = "showtype",
                    label    = NULL,
-                   choices  = list("按方案颜色数量（随机显示配色方案）" = "num", "按配色方案id" = "id", "自定义配色方案" = "custom"),
+                   choices  = list("配色数据库方案id" = "id", "自定义配色方案" = "custom"),
                    inline   = T,
-                   selected = c("按数量" = "num")),
+                   selected = c("按数量" = "id")),
       # 按数量
       conditionalPanel(condition = "input.showtype == 'num'",
                        div(style = "display: flex; align-items: center; width:800px",
@@ -134,7 +140,7 @@ server <- function(input, output, session){
                                                    label = "上一个",
                                                    icon  = icon("angle-left"))),
                            div(style = "width: 15%; text-align: center;", 
-                               shiny::actionButton(inputId = "nex", 
+                               shiny::actionButton(inputId = "hex", 
                                                    label = "下一个",
                                                    icon  = icon("angle-right"))))),
       # 自定义
@@ -153,7 +159,7 @@ server <- function(input, output, session){
           div(style = "width: 50%; text-align: center;", 
               h3("方案样式"),
               plotOutput(outputId = "plot_color", width = "500px", height = "400px"))),
-      h3("配色数据库详情："),
+      h3("配色数据库详情（点击表格显示绘图效果）："),
       reactableOutput(outputId = "colors_db")
     )
   })
@@ -163,43 +169,66 @@ server <- function(input, output, session){
     newValue <- max(1, input$id_select - 1)
     updateSliderInput(session, "id_select", value = newValue)
   })
-  observeEvent(input$nex, {
+  observeEvent(input$hex, {
     newValue <- min(length(colors_nasc), input$id_select + 1)
     updateSliderInput(session, "id_select", value = newValue)
   })
   # 动态数据
-  rv <- reactiveValues()
-  rv$value <- 1
+  rv <- reactiveValues()  #创建一个反应值对象rv，用于存储反应性的数据
+  rv$value <- 1           #初始值为1
   observeEvent(c(input$id_select, input$plot, input$num_select, input$showtype), {
-    # reactive({
+    # 监听多个位置，如果有变动，则运行下面的内容，生成显示颜色方案的id
     if(input$showtype == "id"){
+      # 如果此时页面为按id，则按选择的id更新id
       id <- input$id_select %>% as.numeric()
     } else{
+      # 如果此时页面不是按id，则根据选择颜色数量，随机生成id
       num <- input$num_select %>% as.numeric()
       id  <- sample(colors_table[colors_table$col_num==num,]$col_id, 1)
     }
     rv$value <- id
-  })  
-  
-  # table.1
+  })
+  # table.1：所选配色方案
   output$colors_info  <- renderReactable({
     showtype <- input$showtype
     custtext <- input$col_custom
     if(showtype != "custom"){
+      # 如果非自定义，则按id显示方案信息
       id <- rv$value
-      colo_inf <- data.frame(id=id, colors_n=length(colors_nasc[[id]]), colors=paste0(colors_nasc[[id]], collapse = ", "))
+      colo_inf <- data.frame(id=id, 
+                             colors_n=length(colors_nasc[[id]]), 
+                             colors_hex=paste0(colors_nasc[[id]], collapse = ", "),
+                             colors_show=paste0(colors_nasc[[id]], collapse = ", "))
     } else{
+      # 如果自定义
       colsig <- iscolors(custtext)
       if(isFALSE(colsig)){
-        colo_inf <- data.frame(id=0, colors_n="ERROR", colors="ERROR")
+        # 如果输入的字符不是颜色，则显示ERROR
+        colo_inf <- data.frame(id=0, 
+                               colors_n="ERROR", 
+                               colors_hex="ERROR",
+                               colors_show="FFFFFF")
       } else{
-        colo_inf <- data.frame(id=0, colors_n=length(colsig), colors=paste0(colsig, collapse = ", "))
+        # 如果未输入或输入的字符是颜色，则显示自定义的颜色信息
+        colo_inf <- data.frame(id=0, 
+                               colors_n=length(colsig), 
+                               colors_hex=paste0(colsig, collapse = ", "),
+                               colors_show=paste0(colsig, collapse = ", "))
       }
     }
+    # 显示表格
     reactable(colo_inf,
               columns = list(id=colDef(name="方案id", width = 60, align = "center"),
                              colors_n=colDef(name="所含颜色数", width = 90, align = "center"),
-                             colors=colDef(name = "颜色HEX码", width = 650, align = "center")),
+                             colors_hex=colDef(name = "颜色HEX码", width = 650, align = "center"),
+                             colors_show=colDef(name = "颜色预览", align = "center", cell = function(value) {
+                               color_list <- strsplit(value, ", ")[[1]]
+                               color_divs <- lapply(color_list, function(color) {
+                                 tags$div(style = paste("width: 20px; height: 20px; background-color:", color, "; display: inline-block; margin-right: 5px; border: 1px solid black;"))
+                               })
+                               do.call(tagList, color_divs)
+                             })
+                             ),
               sortable = F, 
               resizable = F,
               showPageSizeOptions = F,
@@ -210,7 +239,7 @@ server <- function(input, output, session){
               width = "1001px",
               fullWidth = F)
   })
-  # fig.1
+  # fig.1：绘图效果
   output$plot_example <- renderPlot({
     showtype <- input$showtype
     custtext <- input$col_custom
@@ -229,8 +258,8 @@ server <- function(input, output, session){
     }
     return(pic)
   }) 
-  # fig.2
-  output$plot_color <- renderPlot({
+  # fig.2：方案样式
+  output$plot_color   <- renderPlot({
     showtype <- input$showtype
     custtext <- input$col_custom
     if(showtype != "custom"){
@@ -248,16 +277,28 @@ server <- function(input, output, session){
     }
     return(pic)
   })  
-  # table.2
-  output$colors_db <- renderReactable({
+  # table.2：配色数据库详情
+  output$colors_db    <- renderReactable({
+    showtype <- input$showtype
     colo_db <- data.frame(id=colors_table$col_id,
                           colors_n=colors_table$col_num,
-                          colors_nex=map_chr(colors_nasc, ~paste0(.x, collapse = ", ")))
+                          colors_hex=map_chr(colors_nasc, ~paste0(.x, collapse = ", ")),
+                          colors_show=map_chr(colors_nasc, ~paste0(.x, collapse = ", ")))
     reactable(colo_db,
               columns = list(id=colDef(name="方案id", width = 60, align = "center"),
                              colors_n=colDef(name="所含颜色数", width = 90, align = "center"),
-                             colors_nex=colDef(name = "颜色HEX码", width = 650, align = "center")),
-              sortable = F, 
+                             colors_hex=colDef(name = "颜色HEX码", width = 650, align = "center"),
+                             colors_show=colDef(name = "颜色预览", align = "center", cell = function(value) {
+                               color_list <- strsplit(value, ", ")[[1]]
+                               color_divs <- lapply(color_list, function(color) {
+                                 tags$div(style = paste("width: 20px; height: 20px; background-color:", color, "; display: inline-block; margin-right: 5px; border: 1px solid black;"))
+                               })
+                               do.call(tagList, color_divs)
+                             })
+                             ),
+              language = reactableLang(
+                searchPlaceholder = "查找..."),
+              sortable = F,
               resizable = F,
               showPageSizeOptions = T,
               searchable = T,
@@ -267,7 +308,23 @@ server <- function(input, output, session){
               compact = F,
               width = "1001px",
               fullWidth = F,
-              theme = reactableTheme(searchInputStyle = list("margin-top" = "7px", "margin-right" = "7px")))
+              theme = reactableTheme(searchInputStyle = list("margin-top" = "7px", "margin-right" = "7px")),
+              onClick = if(showtype != "custom"){
+                JS("function(rowInfo, colInfo) {
+                if (window.Shiny) {
+                  Shiny.setInputValue('selected_row', rowInfo.index + 1);
+                  window.scrollTo(0, 0);  // 滚动到页面顶部
+                }
+                }")}
+            )
+  })
+  
+  observeEvent(input$selected_row, {
+    selected_row <- input$selected_row
+    if (!is.null(selected_row)) {
+      selected_id <- colors_table$col_id[selected_row]
+      updateSliderInput(session, "id_select", value = selected_id)
+    }
   })
 }
 
